270e15d983607f600a4f4c360ab4e4f915834e9a
AlmirPaulo <ap.freelas@gmail.com>
2021-05-15 22:39:15 -0300

    db created

--- MinimalCalendarAPI/__init__.py
+++ MinimalCalendarAPI/__init__.py
@@ -1,10 +1,31 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+import os, logging
 
+#logger set up
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(filename='server.log', format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
+#app and database instances 
+app = Flask(__name__)
+db = SQLAlchemy()
+
+#Factory
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET-KEY'] = 'secret key'
+    from . import views, models, api 
 
+    app.config['SECRET_KEY'] = 'secret key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+    
+    db.init_app(app)
+    from .models import User, Todo
+    create_database(app)
+ 
     return app
+
+def create_database(app):
+    if not os.path.exists('data.db'):
+        db.create_all(app=app)
 
 
---
+++ MinimalCalendarAPI/models.py
@@ -0,0 +1,19 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password  = db.Column(db.String(12), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.String(40), unique=True, nullable=False)
+
+    def __repr__(self): 
+        return '<Todo %r>' % self.task
+
+
